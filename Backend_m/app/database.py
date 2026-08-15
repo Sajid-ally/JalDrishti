96aa6f7ab1d9068fb8ab4445fa2a6fa13153541e
Sajid <mohd.sajid120msd@gmail.com>
2026-08-15 14:55:07 +0530

    Add backend and ML service requirements files

--- Backend_m/app/database.py
+++
@@ -1,8 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from app.config import settings
-
-# Create MongoDB client
-client = AsyncIOMotorClient(settings.MONGO_URI)
-
-# Select database
-database = client[settings.DATABASE_NAME]
